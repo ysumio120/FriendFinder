45f9d6d6b6162c4ff1992037f572d92daaf8dfb7
Yuuta Sumioka <ysumio120@yahoo.com>
2016-11-01 03:00:26 -0400

    Finished basic html (home and survey); Need to finish script for survey.html

---
+++ app/routing/api-routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+	app.get("/api/friends", function(req, res) {
+		
+	});
+
+	app.post("/api/friends", function(req, res) {
+		
+	});
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+app.listen(8080, function() {
+	console.log("app listening on PORT: " + port);
+});
