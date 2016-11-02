a9600e5886f70a2fbaadbe3df2a94b1eb424f9a1
Yuuta Sumioka <ysumio120@yahoo.com>
2016-11-01 21:28:05 -0400

    Fixed parseInt with choices; Finished 'Best Match'

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+module.exports = [
+	{
+		name: "Dummy",
+		image: "",
+		selected: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	}
+]
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,9 +1,41 @@
+var friends = require("../data/friends.js"); // Array holding all current 'friends'
+
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		
+		res.send(friends);
 	});
 
 	app.post("/api/friends", function(req, res) {
-		
+		convertStringtoInt(req.body.selected);
+		console.log(req.body);
+		var bestMatch = null;
+		var minDiff = null;
+	
+		var recentlyAdded = req.body;
+		for(var i = 0; i < friends.length; i++) {	
+			var existingFriend = friends[i];
+			var diff = 0;
+			for(var j = 0; j < 10; j++) {  // 10 questions
+				diff += Math.abs(recentlyAdded.selected[j] - existingFriend.selected[j]);
+			}
+			console.log(diff);
+			if(bestMatch == null && minDiff == null) {
+				bestMatch = existingFriend;
+				minDiff = diff;
+			} 
+			else if(diff < minDiff) {
+				bestMatch = existingFriend;
+				minDiff = diff;
+			}
+		}
+		console.log(bestMatch);
+		friends.push(req.body);
+		// console.log(friends);
 	});
+}
+
+function convertStringtoInt(choices) {
+	for(var i = 0; i < choices.length; i++) {
+		choices[i]= parseInt(choices[i]);
+	}
 }
